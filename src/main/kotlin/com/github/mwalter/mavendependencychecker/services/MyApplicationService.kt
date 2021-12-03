b2c4c58258e7043d136ca97f59b2ad0a34af9a67
GitHub Action <action@github.com>
2021-12-03 14:19:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mwalter/mavendependencychecker/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mwalter.mavendependencychecker.services
+
+import com.github.mwalter.mavendependencychecker.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
