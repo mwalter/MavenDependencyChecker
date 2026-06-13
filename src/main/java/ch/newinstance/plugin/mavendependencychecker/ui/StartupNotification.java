6f2ee7ded7293c3e978dfd2304e49adbce240360
Marc Walter <marc.walter.ch@gmail.com>
2026-06-13 12:49:15 +0200

    remove IdeaPluginDescriptor because of plugin verification error

--- src/main/java/ch/newinstance/plugin/mavendependencychecker/ui/StartupNotification.java
+++ src/main/java/ch/newinstance/plugin/mavendependencychecker/ui/StartupNotification.java
@@ -1,42 +1,50 @@
 package ch.newinstance.plugin.mavendependencychecker.ui;
 
 import ch.newinstance.plugin.mavendependencychecker.config.MavenDependencyCheckerSettings;
-import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.PluginManagerCore;
 import com.intellij.notification.BrowseNotificationAction;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.extensions.PluginId;
-import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.ProjectActivity;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.util.JDOMUtil;
 import com.intellij.util.text.VersionComparatorUtil;
 import kotlin.Unit;
 import kotlin.coroutines.Continuation;
+import org.jdom.Element;
+import org.jdom.JDOMException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class StartupNotification implements ProjectActivity, DumbAware {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+public class StartupNotification implements ProjectActivity {
+
+    private static final String PLUGIN_FILE = "META-INF/plugin.xml";
+
+    private static final String PLUGIN_VERSION_ELEMENT = "version";
 
     @Override
     public @Nullable Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
-        IdeaPluginDescriptor plugin = PluginManagerCore.getPlugin(PluginId.getId("ch.newinstance.plugin.mavendependencychecker"));
         MavenDependencyCheckerSettings settings = ApplicationManager.getApplication().getService(MavenDependencyCheckerSettings.class);
         String installedVersion = settings.getInstalledVersion();
 
-        int compareResult = 0;
-        if (plugin != null) {
-            compareResult = VersionComparatorUtil.compare(installedVersion, plugin.getVersion());
+        Optional<String> versionFromFile = readPluginVersion();
+        if (versionFromFile.isEmpty()) {
+            return null;
         }
 
+        String version = versionFromFile.get();
+        int compareResult = VersionComparatorUtil.compare(installedVersion, version);
         if (compareResult < 0) {
             Notification notification = createNotification();
             notification.notify(null);
-            settings.setInstalledVersion(plugin.getVersion());
+            settings.setInstalledVersion(version);
         }
         return null;
     }
@@ -53,6 +61,21 @@ public class StartupNotification implements ProjectActivity, DumbAware {
                         "Plugin Homepage",
                         "https://plugins.jetbrains.com/plugin/18525-maven-dependency-checker")
                 );
+    }
+
+    private Optional<String> readPluginVersion() {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PLUGIN_FILE);
+
+        try {
+            Element root = JDOMUtil.load(inputStream);
+            if (root != null) {
+                Element version = root.getChild(PLUGIN_VERSION_ELEMENT);
+                return Optional.of(version.getText());
+            }
+            return Optional.empty();
+        } catch (JDOMException | IOException e) {
+            throw new IllegalArgumentException("Error reading file plugin.xml.", e);
+        }
     }
 
 }
